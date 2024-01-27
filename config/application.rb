4c24d7724939d9756342b647d3fd09bfefea9fa2
Cristiano dos Santos <cristianosql02@gmail.com>
2024-01-27 14:19:22 -0300

    feat: add session_store

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module EcommerceApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '__anypasswordhere'
 
     # Configuration for the application, engines, and railties goes here.
     #
