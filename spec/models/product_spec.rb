e5becc335c757bb58b69caef375db94c27ec3479
Cristiano dos Santos <cristianosql02@gmail.com>
2024-01-29 15:09:57 -0300

    feat: criando model polimorfica de produtos e fazendo a primeira cobertura de testes

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   include NameValidatable
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_presence_of(:price) }
+  it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
+  it { is_expected.to belong_to :productable }
 end
